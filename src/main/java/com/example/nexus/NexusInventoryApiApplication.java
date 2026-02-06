d8a0f3de839421021699f7c17102a5b7474f19af
Jhoan Sebastian Urrea Sanchez <120207582+Jhoan-Urrea@users.noreply.github.com>
2026-02-06 13:19:07 -0500

    Creacion del proyecto con spring initializr

---
+++ src/main/java/com/example/nexus/NexusInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusInventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusInventoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nexus/NexusInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
